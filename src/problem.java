e966c9ea7cce63e4d0ac6e92d00dd3deca920c48
wjdwlgkgk <wjdwlgkgk@gmail.com>
2018-12-10 16:37:49 +0900

    Initial commit

---
+++ src/problem.java
@@ -0,0 +1,14 @@
+public class problem {
+    public static void main(String args[]){
+        int sum = 0;
+        int i;
+
+        for (i = 0; i<10; i++)
+        {
+            sum += i;
+        }
+        System.out.println("합계는 : " + sum);
+    }
+
+
+}
